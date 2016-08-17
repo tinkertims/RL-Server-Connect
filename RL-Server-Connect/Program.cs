ace1eb6d8b2eebb6ab56b70202a3903e1a429f54
Tim <timothyunrue@gmail.com>
2016-08-17 00:23:18 -0400

    Initial Commit

---
+++ RL-Server-Connect/Program.cs
@@ -0,0 +1,356 @@
+﻿using Fiddler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RL_Server_Connect
+{
+    class Program
+    {
+        static Proxy oSecureEndpoint;
+        static string sSecureEndpointHostname = "localhost";
+        static int iSecureEndpointPort = 7777;
+        public static bool bDone;
+
+        public static void WriteCommandResponse(string s)
+        {
+            ConsoleColor oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(s);
+            Console.ForegroundColor = oldColor;
+        }
+
+        public static void DoQuit()
+        {
+            WriteCommandResponse("Shutting down...");
+            if (null != oSecureEndpoint) oSecureEndpoint.Dispose();
+            Fiddler.FiddlerApplication.Shutdown();
+        }
+
+        static void Main(string[] args)
+        {
+            String sIpAddress = "127.0.0.1";
+            // Create a timer with a ten second interval.
+            System.Timers.Timer aTimer = new System.Timers.Timer(10000);
+
+            List<Fiddler.Session> oAllSessions = new List<Fiddler.Session>();
+
+            // <-- Personalize for your Application, 64 chars or fewer
+            Fiddler.FiddlerApplication.SetAppDisplayName("FiddlerCoreDemoApp");
+            // Set the Interval to 2 seconds (2000 milliseconds).
+            aTimer.Interval = 30000;
+            aTimer.Enabled = true;
+
+            #region AttachEventListeners
+            //
+            // It is important to understand that FiddlerCore calls event handlers on session-handling
+            // background threads.  If you need to properly synchronize to the UI-thread (say, because
+            // you're adding the sessions to a list view) you must call .Invoke on a delegate on the 
+            // window handle.
+            // 
+            // If you are writing to a non-threadsafe data structure (e.g. List<t>) you must
+            // use a Monitor or other mechanism to ensure safety.
+            //
+
+            // Simply echo notifications to the console.  Because Fiddler.CONFIG.QuietMode=true 
+            // by default, we must handle notifying the user ourselves.
+            Fiddler.FiddlerApplication.OnNotification += delegate (object sender, NotificationEventArgs oNEA) { Console.WriteLine("** NotifyUser: " + oNEA.NotifyString); };
+            Fiddler.FiddlerApplication.Log.OnLogString += delegate (object sender, LogEventArgs oLEA) {
+                if (oLEA.LogString.Contains("psyon"))
+                    Console.WriteLine("** LogString: " + oLEA.LogString);
+
+            };
+
+            Fiddler.FiddlerApplication.BeforeRequest += delegate (Fiddler.Session oS)
+            {
+                // Console.WriteLine("Before request for:\t" + oS.fullUrl);
+                // In order to enable response tampering, buffering mode MUST
+                // be enabled; this allows FiddlerCore to permit modification of
+                // the response in the BeforeResponse handler rather than streaming
+                // the response to the client as the response comes in.
+
+                oS.bBufferResponse = true;
+
+                // Set this property if you want FiddlerCore to automatically authenticate by
+                // answering Digest/Negotiate/NTLM/Kerberos challenges itself
+                oS["X-AutoAuth"] = "(default)";
+
+                /* If the request is going to our secure endpoint, we'll echo back the response.
+                
+                Note: This BeforeRequest is getting called for both our main proxy tunnel AND our secure endpoint, 
+                so we have to look at which Fiddler port the client connected to (pipeClient.LocalPort) to determine whether this request 
+                was sent to secure endpoint, or was merely sent to the main proxy tunnel (e.g. a CONNECT) in order to *reach* the secure endpoint.
+
+                As a result of this, if you run the demo and visit https://localhost:7777 in your browser, you'll see
+
+                Session list contains...
+                 
+                    1 CONNECT http://localhost:7777
+                    200                                         <-- CONNECT tunnel sent to the main proxy tunnel, port 8877
+
+                    2 GET https://localhost:7777/
+                    200 text/html                               <-- GET request decrypted on the main proxy tunnel, port 8877
+
+                    3 GET https://localhost:7777/               
+                    200 text/html                               <-- GET request received by the secure endpoint, port 7777
+                */
+
+                if ((oS.oRequest.pipeClient.LocalPort == iSecureEndpointPort) && (oS.hostname == sSecureEndpointHostname))
+                {
+                    oS.utilCreateResponseAndBypassServer();
+                    oS.oResponse.headers.SetStatus(200, "Ok");
+                    oS.oResponse["Content-Type"] = "text/html; charset=UTF-8";
+                    oS.oResponse["Cache-Control"] = "private, max-age=0";
+                    oS.utilSetResponseBody("<html><body>Request for httpS://" + sSecureEndpointHostname + ":" + iSecureEndpointPort.ToString() + " received. Your request was:<br /><plaintext>" + oS.oRequest.headers.ToString());
+                }
+
+                String request = oS.GetRequestBodyAsString();
+
+                if (oS.fullUrl.Contains("auth") && oS.fullUrl.Contains("psyonix"))
+                {
+                 // Currently you will manually need to login.  Just get 1.18
+                }
+                
+            };
+
+            /*
+               // The following event allows you to examine every response buffer read by Fiddler. Note that this isn't useful for the vast majority of
+               // applications because the raw buffer is nearly useless; it's not decompressed, it includes both headers and body bytes, etc.
+               //
+               // This event is only useful for a handful of applications which need access to a raw, unprocessed byte-stream
+               Fiddler.FiddlerApplication.OnReadResponseBuffer += new EventHandler<RawReadEventArgs>(FiddlerApplication_OnReadResponseBuffer);
+           */
+            Fiddler.FiddlerApplication.BeforeResponse += delegate (Fiddler.Session oS) {
+               if (oS.fullUrl.Contains("INT.txt") && oS.fullUrl.Contains("psyonix"))
+                {
+                    oS.utilDecodeResponse();
+
+                    // Split update and info text
+                    string[] stringSeparators = new string[] { "[Image]", "[Title]", "[Body]", "[MotD]", "[YouTube]", "[Notification]" };
+                    String[] items = oS.GetResponseBodyAsString().Split(stringSeparators, StringSplitOptions.None);
+
+                    for (int i = 0; i < items.Length; i++)
+                    {
+                        // Replace header image
+                        if (i == 0)
+                        {
+                            //oS.utilReplaceOnceInResponse(items[i + 1].Trim(), mainConfig.getHeaderImage(), false);
+                        }
+                        // Replace Title
+                        else if (i == 1)
+                        {
+                            //oS.utilReplaceOnceInResponse(items[i + 1].Trim(), mainConfig.getTitleText(), false);
+                        }
+                        //Repalce Body
+                        else if (i == 2)
+                        {
+                           // oS.utilReplaceOnceInResponse(items[i + 1].Trim(), bodyText, false);
+                        }
+                        // Replace MOTDs
+                        else if (i == 3)
+                        {
+                            //oS.utilReplaceOnceInResponse(items[i + 1].Trim(), mainConfig.getMotds(), false);
+                        }
+                        //Replace YouTube
+                        else if (i == 4)
+                        {
+                            //Console.WriteLine("|" + items[i + 1].Trim() + "|");
+                            //oS.utilReplaceOnceInResponse(items[i + 1].Trim(), mainConfig.getYouTubeText(), false);
+                            //Console.WriteLine(oS.GetResponseBodyAsString());
+                        }
+                        else if (i == 5)
+                        {
+                            if (items[i + 1].Trim() == "") ;
+                            // oS.utilReplaceInResponse("[Notification]", "[Notification]" + mainConfig.getNotificationText());
+                            else;
+                                //oS.utilReplaceInResponse(items[i + 1].Trim(), mainConfig.getNotificationText());
+
+                        }
+                    }
+
+                    //oS.utilReplaceInResponse("http://rl-cdn.psyonix.com/Blog/Images/RLCS_Announcement.jpg", "http://i.imgur.com/d3DgIhC.jpg");
+
+                    //oS.utilReplaceInResponse("[Notification]", "[Notification]\nWHAT THIS DO?");
+                    //Console.WriteLine(oS.GetResponseBodyAsString());
+                }
+
+                String request = oS.GetRequestBodyAsString();
+
+                if (oS.fullUrl.Contains("auth") && oS.fullUrl.Contains("psyonix"))
+                {
+                    oS.utilDecodeResponse();
+
+
+
+                }
+
+                if (oS.fullUrl.Contains("callproc105") && request.Contains("GetCustomGameServerList02"))
+                {
+                    oS.utilDecodeResponse();
+                    oS.utilSetResponseBody("IP=" + sIpAddress + ":7778&ServerName=NameDoesNotMatter&PlaylistID=6");
+
+
+                }                
+                
+            };
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(Console_CancelKeyPress);
+            #endregion AttachEventListeners
+
+            Console.WriteLine(String.Format("Starting {0} ({1})...", Fiddler.FiddlerApplication.GetVersionString(), "NoSAZ"));
+
+            // For the purposes of this demo, we'll forbid connections to HTTPS 
+            // sites that use invalid certificates. Change this from the default only
+            // if you know EXACTLY what that implies.
+            Fiddler.CONFIG.IgnoreServerCertErrors = false;
+
+            // ... but you can allow a specific (even invalid) certificate by implementing and assigning a callback...
+            FiddlerApplication.OnValidateServerCertificate += new System.EventHandler<ValidateServerCertificateEventArgs>(CheckCert);
+
+            //FiddlerApplication.Prefs.SetBoolPref("fiddler.network.streaming.abortifclientaborts", true);
+
+            // For forward-compatibility with updated FiddlerCore libraries, it is strongly recommended that you 
+            // start with the DEFAULT options and manually disable specific unwanted options.
+            FiddlerCoreStartupFlags oFCSF = FiddlerCoreStartupFlags.Default;
+
+            // E.g. If you want to add a flag, start with the .Default and "OR" the new flag on:
+            // oFCSF = (oFCSF | FiddlerCoreStartupFlags.CaptureFTP);
+
+            // ... or if you don't want a flag in the defaults, "and not" it out:
+            // Uncomment the next line if you don't want FiddlerCore to act as the system proxy
+            // oFCSF = (oFCSF & ~FiddlerCoreStartupFlags.RegisterAsSystemProxy);
+
+            // *******************************
+            // Important HTTPS Decryption Info
+            // *******************************
+            // When FiddlerCoreStartupFlags.DecryptSSL is enabled, you must include either
+            //
+            //     MakeCert.exe
+            //
+            // *or*
+            //
+            //     CertMaker.dll
+            //     BCMakeCert.dll
+            //
+            // ... in the folder where your executable and FiddlerCore.dll live. These files
+            // are needed to generate the self-signed certificates used to man-in-the-middle
+            // secure traffic. MakeCert.exe uses Windows APIs to generate certificates which
+            // are stored in the user's \Personal\ Certificates store. These certificates are
+            // NOT compatible with iOS devices which require specific fields in the certificate
+            // which are not set by MakeCert.exe. 
+            //
+            // In contrast, CertMaker.dll uses the BouncyCastle C# library (BCMakeCert.dll) to
+            // generate new certificates from scratch. These certificates are stored in memory
+            // only, and are compatible with iOS devices.
+
+            // Uncomment the next line if you don't want to decrypt SSL traffic.
+            // oFCSF = (oFCSF & ~FiddlerCoreStartupFlags.DecryptSSL);
+
+            // NOTE: In the next line, you can pass 0 for the port (instead of 8877) to have FiddlerCore auto-select an available port
+            int iPort = 8877;
+            Fiddler.FiddlerApplication.Startup(iPort, oFCSF);
+
+            FiddlerApplication.Log.LogFormat("Created endpoint listening on port {0}", iPort);
+
+            FiddlerApplication.Log.LogFormat("Starting with settings: [{0}]", oFCSF);
+            FiddlerApplication.Log.LogFormat("Gateway: {0}", CONFIG.UpstreamGateway.ToString());
+
+            Console.WriteLine("Hit CTRL+C to end session.");
+
+            // We'll also create a HTTPS listener, useful for when FiddlerCore is masquerading as a HTTPS server
+            // instead of acting as a normal CERN-style proxy server.
+            oSecureEndpoint = FiddlerApplication.CreateProxyEndpoint(iSecureEndpointPort, true, sSecureEndpointHostname);
+            if (null != oSecureEndpoint)
+            {
+                FiddlerApplication.Log.LogFormat("Created secure endpoint listening on port {0}, using a HTTPS certificate for '{1}'", iSecureEndpointPort, sSecureEndpointHostname);
+            }
+
+            try
+            {
+                Program.WriteCommandResponse("Result: " + Fiddler.CertMaker.trustRootCert().ToString());
+            }
+            catch (Exception eX)
+            {
+                Program.WriteCommandResponse("Failed: " + eX.ToString());
+            }
+
+            bDone = false;
+            do
+            {
+                Console.WriteLine("\nEnter a command [C=Clear; L=List; G=Collect Garbage; W=write SAZ; R=read SAZ;\n\tS=Toggle Forgetful Streaming; T=Trust Root Certificate; Q=Quit]:");
+                Console.Write(">");
+                ConsoleKeyInfo cki = Console.ReadKey();
+                Console.WriteLine();
+                switch (Char.ToLower(cki.KeyChar))
+                {
+                    case 'd':
+                        FiddlerApplication.Log.LogString("FiddlerApplication::Shutdown.");
+                        FiddlerApplication.Shutdown();
+                        break;
+
+                    case 'g':
+                        Console.WriteLine("Working Set:\t" + Environment.WorkingSet.ToString("n0"));
+                        Console.WriteLine("Begin GC...");
+                        GC.Collect();
+                        Console.WriteLine("GC Done.\nWorking Set:\t" + Environment.WorkingSet.ToString("n0"));
+                        break;
+
+                    case 'q':
+                        bDone = true;
+                        DoQuit();
+                        break;
+
+                    case 't':
+                        try
+                        {
+                            WriteCommandResponse("Result: " + Fiddler.CertMaker.trustRootCert().ToString());
+                        }
+                        catch (Exception eX)
+                        {
+                            WriteCommandResponse("Failed: " + eX.ToString());
+                        }
+                        break;
+
+                    // Forgetful streaming
+                    case 's':
+                        bool bForgetful = !FiddlerApplication.Prefs.GetBoolPref("fiddler.network.streaming.ForgetStreamedData", false);
+                        FiddlerApplication.Prefs.SetBoolPref("fiddler.network.streaming.ForgetStreamedData", bForgetful);
+                        Console.WriteLine(bForgetful ? "FiddlerCore will immediately dump streaming response data." : "FiddlerCore will keep a copy of streamed response data.");
+                        break;
+
+                }
+            } while (!bDone);
+
+        }
+
+        /// <summary>
+        /// This callback allows your code to evaluate the certificate for a site and optionally override default validation behavior for that certificate.
+        /// You should not implement this method unless you understand why it is a security risk.
+        /// </summary>
+        static void CheckCert(object sender, ValidateServerCertificateEventArgs e)
+        {
+            if (null != e.ServerCertificate)
+            {
+                //Console.WriteLine("Certificate for " + e.ExpectedCN + " was for site " + e.ServerCertificate.Subject + " and errors were " + e.CertificatePolicyErrors.ToString());
+
+                //if (e.ServerCertificate.Subject.Contains("fiddler2.com"))
+                //{
+                //Console.WriteLine("Got a certificate for fiddler2.com. We'll say this is also good for any other site, like https://fiddlertool.com.");
+                e.ValidityState = CertificateValidity.ForceValid;
+                // }
+            }
+        }
+
+        /// <summary>
+        /// When the user hits CTRL+C, this event fires.  We use this to shut down and unregister our FiddlerCore.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            DoQuit();
+        }
+
+    }
+}
